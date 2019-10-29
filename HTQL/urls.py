17105e4112f9ebff5adaff585516a41145c5b78d
baphuc1998 <baphucb1605354@gmail.com>
2019-10-29 20:14:01 +0700

    delete jwt

--- HTQL/urls.py
+++ HTQL/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from rest_framework_jwt.views import obtain_jwt_token
+# from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
